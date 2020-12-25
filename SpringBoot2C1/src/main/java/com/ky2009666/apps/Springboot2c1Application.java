32dd3f0e08fdaa91d3c1616ade23a489f2e0ce79
cf_jijinliang <ji_jinliang2012@163.com>
2020-12-25 17:57:58 +0800

    提交代码

---
+++ SpringBoot2C1/src/main/java/com/ky2009666/apps/Springboot2c1Application.java
@@ -0,0 +1,13 @@
+package com.ky2009666.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2c1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2c1Application.class, args);
+	}
+
+}
