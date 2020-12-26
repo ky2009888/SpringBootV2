e0740e777364bd8e99a9e7016e3c2f5c093dd8e4
cf_jijinliang <ji_jinliang2012@163.com>
2020-12-26 22:11:50 +0800

    新增打印spring boot中已经定义好的组件的名称

--- SpringBoot2C1/src/main/java/com/ky2009666/apps/Springboot2c1Application.java
+++ SpringBoot2C1/src/main/java/com/ky2009666/apps/Springboot2c1Application.java
@@ -1,16 +1,25 @@
 package com.ky2009666.apps;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author Lenovo
  */
 @SpringBootApplication
+@Slf4j
 public class Springboot2c1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot2c1Application.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(Springboot2c1Application.class, args);
+        //打印spring boot中已经定义好的bean组件的名称
+        String[] beanDefinitionNames = run.getBeanDefinitionNames();
+        log.info("打印已经定义好的bean组件的名称:");
+        for (String beanName : beanDefinitionNames) {
+            log.info(beanName);
+        }
+    }
 
 }
