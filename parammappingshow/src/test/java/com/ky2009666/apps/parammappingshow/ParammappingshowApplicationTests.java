95334a08bf006eb62320eb9eb2bdcae47037ee6c
cf_jijinliang <ji_jinliang2012@163.com>
2020-12-29 16:38:23 +0800

    路径参数的使用方法展示

---
+++ parammappingshow/src/test/java/com/ky2009666/apps/parammappingshow/ParammappingshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ky2009666.apps.parammappingshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParammappingshowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
