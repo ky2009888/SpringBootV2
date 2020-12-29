b16b5e3cef79a4ee303ac17ffa2a604043fdce06
cf_jijinliang <ji_jinliang2012@163.com>
2020-12-29 09:42:57 +0800

    yml文件的配置属性的时候，会自动提示，使用插件：spring-boot-configuration-processor

---
+++ Show/src/main/java/app/Show.java
@@ -0,0 +1,14 @@
+package app;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 功能描述:
+ * 项目名称:SpringBootV2
+ * 创建日期:2020/12/28
+ */
+@Slf4j
+public class Show {
+
+}
