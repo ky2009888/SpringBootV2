e905901caa49711ca988320e55ae5eab06b8d0bd
cf_jijinliang <ji_jinliang2012@163.com>
2020-12-29 17:02:18 +0800

    路径参数的使用方法展示

--- parammappingshow/src/main/java/com/ky2009666/apps/parammappingshow/controller/ParamShowController.java
+++ parammappingshow/src/main/java/com/ky2009666/apps/parammappingshow/controller/ParamShowController.java
@@ -2,7 +2,9 @@ package com.ky2009666.apps.parammappingshow.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,5 +29,31 @@ public class ParamShowController {
         userMap.put("age", "19");
         userMap.put("name1", name1);
         return userMap;
+    }
+    //  car/2/owner/zhangsan
+    @GetMapping("/car/{id}/owner/{username}")
+    public Map<String,Object> getCar(@PathVariable("id") Integer id,
+                                     @PathVariable("username") String name,
+                                     @PathVariable Map<String,String> pv,
+                                     @RequestHeader("User-Agent") String userAgent,
+                                     @RequestHeader Map<String,String> header,
+                                     @RequestParam("age") Integer age,
+                                     @RequestParam("inters") List<String> inters,
+                                     @RequestParam Map<String,String> params,
+                                     @CookieValue("_ga") String _ga,
+                                     @CookieValue("_ga") Cookie cookie){
+        Map<String,Object> map = new HashMap<>();
+
+//        map.put("id",id);
+//        map.put("name",name);
+//        map.put("pv",pv);
+//        map.put("userAgent",userAgent);
+//        map.put("headers",header);
+        map.put("age",age);
+        map.put("inters",inters);
+        map.put("params",params);
+        map.put("_ga",_ga);
+        System.out.println(cookie.getName()+"===>"+cookie.getValue());
+        return map;
     }
 }
