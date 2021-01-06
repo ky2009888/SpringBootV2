7067c2be8dd45a1dc987674958e0a59303270549
cf_jijinliang <ji_jinliang2012@163.com>
2021-01-06 17:41:00 +0800

    提交springboot2集成mybatis-plus

---
+++ SpringBoot2MybatisPlus/src/main/java/com/apps/SpringBootMybatisPlusMain.java
@@ -0,0 +1,19 @@
+package com.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lenovo
+ */
+@SpringBootApplication
+public class SpringBootMybatisPlusMain {
+    /**
+     * 执行的入口方法
+     *
+     * @param args 参数.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisPlusMain.class, args);
+    }
+}
